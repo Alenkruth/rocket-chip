e07b8d7e8a06e0d4cdf7d29037f99aa64e53684b
alenkruth <alenkruth@gmail.com>
2025-10-28 19:21:37 -0400

    add module specific tags

--- src/main/scala/util/CoreFuzzing.scala
+++ src/main/scala/util/CoreFuzzing.scala
@@ -14,8 +14,31 @@ trait CoreFuzzingConstants{
     // counter for uops
     val uopIDCounterWidthCF = 8
 
+    // tags for each module
+    val numModules = 19
     // tainting info for uops
-    val moduleCountCF = 4 // we will probably have 16 modules tops
+    val moduleCountCF = log2Ceil(numModules) // we will probably have 16 modules tops
+   
+    val icacheTagCF = 0
+    val itlbTagCF = 1
+    val l0BTBTagCF = 2
+    val l1BTBTagCF = 3
+    val btbTagCF = 4
+    val bpdTagCF = 5
+    val rasTagCF = 6
+    val ftqTagCF = 7
+    val fbTagCF = 8
+    val irfTagCF = 9
+    val frfTagCF = 10
+    val intissqTagCF = 11
+    val fpissqTagCF = 12
+    val memissqTagCF = 13
+    val robTagCF = 14
+    val ldqTagCF = 15
+    val stqTagCF = 16
+    val dtlbTagCF = 17
+    val dcacheTagCF = 18
+
     val taintTypeCf = 2 // we have 4 types of taints
 
     // Bit widths of reconfiguration control wires
@@ -23,15 +46,35 @@ trait CoreFuzzingConstants{
     val dcacheParamsWidthCF = 8 
 
     // protected address range
-    val PROTECTED_START = 0x80002c10L
-    val PROTECTED_END   = 0x80002c40L
-
+    // moved to a CSR
+    // val PROTECTED_START = 0x80002c10L
+    // val PROTECTED_END   = 0x80002c40L
     // The L here indicates it is LOOOONG!
     
     // custom ROB entry options
-    // 130 is the default ROB size
-    // def robEntryOptions = Seq(130, 16, 20, 30, 32, 40, 50, 64, 80, 90, 96, 128, 150, 250)
-    def robEntryOptions = Seq(130, 250, 16, 20, 30, 32, 40, 50, 64, 80, 90, 96, 128, 130, 150, 200)
+    // 300 is the default CoreFuzzing ROB size
+    // the rob cofig register at 0xbc2 is by default set to 0 (300 entries)
+    // possible entry configs are
+    // 0 : 300
+    // 1 : 16
+    // 2 : 20
+    // 3 : 24
+    // 4 : 30
+    // 5 : 32
+    // 6 : 40
+    // 7 : 50
+    // 8 : 60
+    // 9 : 64
+    // 10: 80
+    // 11: 90
+    // 12: 96
+    // 13: 128
+    // 14: 130
+    // 15: 150
+    // 16: 200
+    // 17: 250
+    // 18: 256 
+    def robEntryOptions = Seq(300, 16, 20, 24, 30, 32, 40, 50, 60, 64, 80, 90, 96, 128, 130, 150, 200, 250, 256)
     val setOptions = Seq(128, 32, 64)
     val wayOptions = Seq(16, 1, 2, 4, 8)
     val sizeOptions = Seq(128, 2, 4, 8, 16, 32, 64)
@@ -49,10 +92,12 @@ class Tagger (implicit val p: Parameters) extends Module
         val tag_o = Output(UInt(iftTagWidth.W))
     })
 
-    when ((io.addr_i >= PROTECTED_START.U(coreMaxAddrBits.W)) && 
-          (io.addr_i <= PROTECTED_END.U(coreMaxAddrBits.W))){
-        io.tag_o := 1.U(iftTagWidth.W)
-    } .otherwise {
-        io.tag_o := 0.U(iftTagWidth.W)
-    }
+    // when ((io.addr_i >= PROTECTED_START.U(coreMaxAddrBits.W)) && 
+    //       (io.addr_i <= PROTECTED_END.U(coreMaxAddrBits.W))){
+    //     io.tag_o := 1.U(iftTagWidth.W)
+    // } .otherwise {
+    //     io.tag_o := 0.U(iftTagWidth.W)
+    // }
+    // TODO - fix this for tagging if needed
+    io.tag_o := 0.U(iftTagWidth.W) // default no tagging
 }
